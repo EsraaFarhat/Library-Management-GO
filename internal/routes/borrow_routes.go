82e9fd203a54e462cceee9099f268869f624c623
EsraaFarhat <esraafarhat97@gmail.com>
2025-03-27 06:13:27 +0200

    update routes to align with rest APIs prinicples

--- internal/routes/borrow_routes.go
+++ internal/routes/borrow_routes.go
@@ -13,12 +13,12 @@ func SetupBorrowRoutes(r *gin.Engine, borrowHandler *handlers.BorrowHandler) {
 	{
 		borrowRoutes.Use(middlewares.AuthMiddleware())
 		borrowRoutes.POST("/", borrowHandler.BorrowBook)
-		borrowRoutes.POST("/return", borrowHandler.ReturnBook)
+		borrowRoutes.PATCH("/return", borrowHandler.ReturnBook)
 		// Get borrowed books for the logged-in user
-		borrowRoutes.GET("/my-borrows", borrowHandler.GetMyBorrows)
+		borrowRoutes.GET("/", borrowHandler.GetMyBorrows)
 
 		borrowRoutes.Use(middlewares.RoleMiddleware(string(constants.Admin)))
-		borrowRoutes.GET("/", borrowHandler.GetBorrowRecords)
-		borrowRoutes.GET("/user/:user_id", borrowHandler.GetUserBorrows)
+		// borrowRoutes.GET("/", borrowHandler.GetBorrowRecords)
+		borrowRoutes.GET("/users/:user_id", borrowHandler.GetUserBorrows)
 	}
 }
